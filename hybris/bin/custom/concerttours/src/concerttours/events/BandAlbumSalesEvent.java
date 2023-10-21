a0d3fd637e4ad42e8b95fdae99645f43bd1afe45
vineethvangala <myjobapps.vineeth@outlook.in>
2023-10-21 14:24:00 +0530

    Step 15 - Cluster Aware events

--- hybris/bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEvent.java
+++ hybris/bin/custom/concerttours/src/concerttours/events/BandAlbumSalesEvent.java
@@ -1,7 +1,10 @@
 package concerttours.events;
+
+import de.hybris.platform.servicelayer.event.ClusterAwareEvent;
 import de.hybris.platform.servicelayer.event.events.AbstractEvent;
- 
-public class BandAlbumSalesEvent extends AbstractEvent
+
+
+public class BandAlbumSalesEvent extends AbstractEvent implements ClusterAwareEvent
 {
     private final String code;
     private final String name;
@@ -30,4 +33,10 @@ public class BandAlbumSalesEvent extends AbstractEvent
     {
         return this.name;
     }
+
+	 @Override
+	 public boolean publish(final int sourceNodeId, final int targetNodeId)
+	 {
+		 return (sourceNodeId == targetNodeId);
+	 }
 }
